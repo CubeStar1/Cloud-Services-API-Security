f454f646ecada6100b174f40de572c58cd28469a
CubeStar1 <143164352+CubeStar1@users.noreply.github.com>
2026-01-28 21:12:09 +0530

    Fix: iframe link

--- casb/frontend/app/(default)/secure-file-sharing/page.tsx
+++ casb/frontend/app/(default)/secure-file-sharing/page.tsx
@@ -3,7 +3,7 @@
 export default function SecureFileSharing() {
     return (
         <div className=" h-screen">
-            <iframe src="http://127.0.0.1:5000" className="w-full h-full"></iframe>
+            <iframe src="https://98521c0ad2d3.ngrok-free.app" className="w-full h-full"></iframe>
         </div>
     )
 }
